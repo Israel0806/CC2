44396b05f711ed1ed6cec1cb0d7e00ad2d133c12
israel lopez cruz <israel.lopez@gmail.com>
2017-08-23 22:02:50 -0500

    Ejercicio 3

---
+++ Ejercicio 3/main.cpp
@@ -0,0 +1,119 @@
+#include <iostream>
+#include <math.h>
+#include <vector>
+using namespace std;
+
+void calcular(int &a, int &b)
+{
+    bool cond=false;
+        if(a<-b)
+        {
+            a*=-1;
+            cond=true;
+        }
+        if(a>=b)
+        {
+            a=a%b;
+
+            if(cond)
+                a*=-1;
+        }
+        if(a<0)
+        {
+            a=b+a;
+        }
+}
+
+int dividir(int &a, int &b)
+{
+	if(a==0)
+		return a;
+	int d=a;
+	a=b%a;
+	b=d;
+	if(b<0)
+		b*=-1;
+	return dividir(a,b);
+}
+
+void inversa(int a, int b)
+{
+    int c=b;
+    vector<int> q, p;
+    p.push_back(0);
+    p.push_back(1);
+    int n=0;
+    while(a!=0)
+    {
+        int d=a;
+        q.push_back(b/a);
+        a=b%a;
+        b=d;
+        n++;
+    }
+    int x=2;
+    int temp;
+    while(x!=n+1)
+    {
+        temp=p[x-2]-(q[x-2]*p[x-1]);
+        calcular(temp,c);
+        cout<<"P"<<x<<": "<<temp<<endl;
+        p.push_back(temp);
+        x++;
+    }
+    cout<<"Inversa: "<<p[x-1]<<" mod "<<c<<endl;
+
+}
+
+class Modulo {
+    int a,b;
+public:
+    Modulo() {}
+    Modulo(int _a, int _b) :a(_a), b(_b) {}
+    Modulo(const Modulo &A) { a=A.a; b=A.b; }
+    int getA() { return a; }
+    int getB() { return b; }
+
+    friend Modulo operator +(Modulo A, Modulo B)
+    {
+        Modulo c;
+        c.a=A.a+B.a;
+        c.b=A.b;
+        calcular(c.a,c.b);
+        return c;
+    }
+    friend Modulo operator -(Modulo A, Modulo B)
+    {
+        Modulo c;
+        c.a=A.a-B.a;
+        c.b=A.b;
+        calcular(c.a,c.b);
+        return c;
+    }
+    friend Modulo operator *(Modulo A, Modulo B)
+    {
+        Modulo c;
+        c.a=A.a*B.a;
+        c.b=A.b;
+        calcular(c.a,c.b);
+        return c;
+    }
+    void Inverso()
+    {
+        Modulo m(a,b);
+        dividir(m.a,m.b);
+        if(m.b==1)
+        {
+            inversa(a,b);
+        }
+    }
+};
+
+
+int main()
+{
+    Modulo a(29,256), b(40,6);
+    a.Inverso();
+
+    return 0;
+}
